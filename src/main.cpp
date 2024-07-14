97ddd0118aded811d9a219a18a20496cb298e3ef
Kosmonautt <b.contreras.2@ug.uchile.cl>
2024-07-14 01:06:31 -0400

    moved functions around

--- src/main.cpp
+++ src/main.cpp
@@ -13,42 +13,6 @@ typedef K::Ray_2 Ray_2;
 typedef K::Line_2 Line_2;
 typedef CGAL::Delaunay_triangulation_2<K>  Delaunay_triangulation_2;
 
-// struct that will store the cropped Voronoi diagram
-struct Cropped_voronoi_from_delaunay{
-    // this list will store the segments of the cropped Voronoi diagram
-    std::list<Segment_2> m_cropped_vd;
-    // this is the bounding box that will constrain the Voronoi diagram
-    Iso_rectangle_2 m_bbox;
-
-    // constructor that receives the bounding box
-    Cropped_voronoi_from_delaunay(const Iso_rectangle_2& bbox):m_bbox(bbox){}
-
-    // this template allows to use the same function for rays, lines and segments
-    template <class RSL>
-    // this function crops the input object to the bounding box and stores the segment
-    void crop_and_extract_segment(const RSL& rsl){
-        // the intersection of the rsl and the bounding box is stored in obj which supports multiple types
-        CGAL::Object obj = CGAL::intersection(rsl,m_bbox);
-        // if obj is a segment, it is stored in the list
-        const Segment_2* s=CGAL::object_cast<Segment_2>(&obj);
-        if (s) 
-            m_cropped_vd.push_back(*s);
-    }
-
-    // overload the << operator to allow the cropping of rays, lines and segments
-    void operator<<(const Ray_2& ray) {
-        crop_and_extract_segment(ray);
-    }
-
-    void operator<<(const Line_2& line) {
-        crop_and_extract_segment(line); 
-    }
-
-    void operator<<(const Segment_2& seg) {
-        crop_and_extract_segment(seg); 
-    }
-};
-
 // vertex shader source
 const char* vertexShaderSource = R"glsl(
     #version 330 core
@@ -74,7 +38,6 @@ unsigned int compileShader(unsigned int type, const char* source) {
     glCompileShader(id);
     return id;
 }
-
 
 // create shader program
 unsigned int createShaderProgram(const char* vertexShaderSrc, const char* fragmentShaderSrc) {
@@ -121,6 +84,42 @@ GLFWwindow* initWindowAndContext() {
     }
     return window;
 }
+
+// struct that will store the cropped Voronoi diagram
+struct Cropped_voronoi_from_delaunay{
+    // this list will store the segments of the cropped Voronoi diagram
+    std::list<Segment_2> m_cropped_vd;
+    // this is the bounding box that will constrain the Voronoi diagram
+    Iso_rectangle_2 m_bbox;
+
+    // constructor that receives the bounding box
+    Cropped_voronoi_from_delaunay(const Iso_rectangle_2& bbox):m_bbox(bbox){}
+
+    // this template allows to use the same function for rays, lines and segments
+    template <class RSL>
+    // this function crops the input object to the bounding box and stores the segment
+    void crop_and_extract_segment(const RSL& rsl){
+        // the intersection of the rsl and the bounding box is stored in obj which supports multiple types
+        CGAL::Object obj = CGAL::intersection(rsl,m_bbox);
+        // if obj is a segment, it is stored in the list
+        const Segment_2* s=CGAL::object_cast<Segment_2>(&obj);
+        if (s) 
+            m_cropped_vd.push_back(*s);
+    }
+
+    // overload the << operator to allow the cropping of rays, lines and segments
+    void operator<<(const Ray_2& ray) {
+        crop_and_extract_segment(ray);
+    }
+
+    void operator<<(const Line_2& line) {
+        crop_and_extract_segment(line); 
+    }
+
+    void operator<<(const Segment_2& seg) {
+        crop_and_extract_segment(seg); 
+    }
+};
 
 int main(int, char**) {
     // Initialize window and context
